beed65d359e487a54c2bf2815f5d94aac190cb8a
zhouweibing <zhouweibing@skrer.net>
2019-09-18 20:59:31 +0800

    变为一次引入

--- index.js
+++ index.js
@@ -4,6 +4,21 @@
 import AppForm from './src/components/app-form'
 import AppTable from './src/components/app-table'
 import AppFormItem from './src/components/app-form-item'
+const components = [
+    AppForm,
+    AppTable,
+    AppFormItem
+]
+const install = function(Vue, opts = {}) {
+    components.forEach(component => {
+      Vue.component(component.name, component);
+    });
+  }
+  
+/* istanbul ignore if */
+if (typeof window !== 'undefined' && window.Vue) {
+    install(window.Vue);
+  }
 
 export default {
     AppForm,
