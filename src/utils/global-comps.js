00e12ceb8294431a81c8290e72fd38ffb137738d
zhouweibing <zhouweibing@skrer.net>
2019-09-19 10:20:54 +0800

    组件变为一次引入

--- src/utils/global-comps.js
+++ src/utils/global-comps.js
@@ -2,21 +2,12 @@
  * 全局祖册组件 elementUI 自定义组件
  */
 import Vue from 'vue'
-// import Cookies from 'js-cookie'
-// import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-// import '@/styles/index.scss' // global css
-// import '../icons' // icon
-// import '../permission' // permission control
-// import './mock' // simulation data
-// import moment from 'moment'
-// import ZqComponents from 'zq-components'
 import request from './request'
 import AppTable from '../components/app-table'
 import AppForm from '../components/app-form'
 import AppFormItem from '../components/app-form-item'
-// Vue.prototype.$moment = moment()
 Vue.prototype.request = request
 const components = [
   AppForm,
