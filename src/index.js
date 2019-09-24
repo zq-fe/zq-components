cae79028bcb947bde801ae841a6b45107d825fc3
zhouweibing <zhouweibing@skrer.net>
2019-09-24 19:19:32 +0800

    全局注册方式修改

--- src/index.js
+++ src/index.js
@@ -1,23 +1,4 @@
 /**
  * 全局祖册组件 elementUI 自定义组件
  */
-import Vue from 'vue'
-import AppForm from './components/app-form'
-import AppTable from './components/app-table'
-import AppFormItem from './components/app-form-item'
-import { ImageUpload, AudioUpload } from './components/app-upload';
-
-const components = [
-    AppForm,
-    AppTable,
-    AppFormItem
-]
-Vue.component('image-upload', ImageUpload);
-Vue.component('audio-upload', AudioUpload);
-export default {
-    install(Vue, opts = {}) {
-        components.forEach(component => {
-          Vue.component(component.name, component);
-        });
-      }
-};
+import './utils/global-comps';
