608560973a3cf51e61efa7eaa7c863ecbe3d7d21
zhouweibing <zhouweibing@skrer.net>
2019-09-27 11:48:47 +0800

    溢出显示

--- src/components/app-table/column-cell/items/Text.vue
+++ src/components/app-table/column-cell/items/Text.vue
@@ -6,11 +6,10 @@
   <el-popover
     v-else
     placement="top-start"
-    title="标题"
     width="200"
     trigger="hover"
     :content="text">
-    <el-button slot="reference">{{ text.substring(0,9) }}...</el-button>
+    <span slot="reference">{{ text.substring(0,9) }}...</span>
   </el-popover>
 </div>
 </template>
@@ -22,7 +21,8 @@ export default {
   mixins: [Mixins],
   computed: {
     textLength () {
-      return this.record[this.column.name].length
+      const name = this.record[this.column.name]
+      return typeof name === 'string' ? name.length : 1
     },
     text () {
       return this.record[this.column.name] || ''
