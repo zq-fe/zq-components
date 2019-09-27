abea21c37ff647137162bac41e79e131bbc17966
zhouweibing <zhouweibing@skrer.net>
2019-09-27 11:27:48 +0800

    溢出省略

--- src/components/app-table/column-cell/items/Text.vue
+++ src/components/app-table/column-cell/items/Text.vue
@@ -1,14 +1,33 @@
 <template>
-  <div class="cell-text">
-    {{ record[column.name] | filter(record, column) }}
+<div>
+  <div v-if="textLength < 10" class="cell-text">
+    {{ text | filter(record, column) }}
   </div>
+  <el-popover
+    v-else
+    placement="top-start"
+    title="标题"
+    width="200"
+    trigger="hover"
+    :content="text">
+    <el-button slot="reference">{{ text.substring(0,9) }}...</el-button>
+  </el-popover>
+</div>
 </template>
 
 <script>
 import Mixins from '../mixins';
 export default {
   name: 'ColumnCellText',
-  mixins: [Mixins]
+  mixins: [Mixins],
+  computed: {
+    textLength () {
+      return record[column.name].length
+    },
+    text () {
+      return record[column.name] || ''
+    }
+  }
 }
 </script>
 
