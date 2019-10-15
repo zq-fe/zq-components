078afa0f10e9a2185914b5a3c9cfae77209696a8
zhouweibing <zhouweibing@skrer.net>
2019-10-15 19:45:50 +0800

    更新bug

--- src/components/app-excel/Excel.vue
+++ src/components/app-excel/Excel.vue
@@ -1,5 +1,11 @@
 <template>
-  <el-button v-if="data.export" type="success" icon="el-icon-bottom" @click="exportToExcel">导出</el-button>
+  <el-button
+    v-if="data.export"
+    type="success"
+    icon="el-icon-bottom"
+    @click="exportToExcel"
+    :loading="loading"
+  >导出</el-button>
 </template>
 <script>
 /**
@@ -7,15 +13,15 @@
  * @displayName App-Form
  */
 import ExportJsonExcel from "js-export-excel";
-import './filters';
-import Vue from 'vue'
+import "./filters";
+import Vue from "vue";
 export default {
   name: "AppExcel",
   props: {
-     query: {
+    query: {
       type: Object,
       default() {
-        return {}
+        return {};
       }
     },
     /**
@@ -79,7 +85,6 @@ export default {
         offset: pagination.offset,
         cnt: pagination.cnt
       };
-      this.loading = true;
       const requestOptions = {
         url,
         method: "get",
@@ -94,14 +99,15 @@ export default {
       for (let i = 0; i < data.length; i++) {
         let temp = [];
         for (let j = 0; j < this.sheetHeader.length; j++) {
-          const record = data[i]
-          let item = record[this.sheetHeader[j]]
-          const filter = columns[j] && columns[j].filter ? columns[j].filter : ''
-          if (filter && typeof filter === 'function') {
-            item = filter(item)
-          } else if(filter && typeof filter === 'string') {
-            const filterFunc = Vue.filter(filter)
-            item = filterFunc(item,record,columns[j])
+          const record = data[i];
+          let item = record[this.sheetHeader[j]];
+          const filter =
+            columns[j] && columns[j].filter ? columns[j].filter : "";
+          if (filter && typeof filter === "function") {
+            item = filter(item);
+          } else if (filter && typeof filter === "string") {
+            const filterFunc = Vue.filter(filter);
+            item = filterFunc(item, record, columns[j]);
           }
           temp.push(item);
         }
@@ -113,20 +119,26 @@ export default {
      * 表单提交，触发 submit
      */
     async getDataList(urls) {
+      this.loading = true;
       const total = Math.ceil(this.pagination.total / this.pagination.pageSize);
       let res = [];
       for (let i = 1; i <= total; i++) {
         this.page = i;
-        const response = await this.searchRequest();
-        const items = await response.data.items;
-        this.dataList = this.dataList.concat(items);
+        try {
+          const response = await this.searchRequest();
+          const items = await response.data.items;
+          this.dataList = this.dataList.concat(items);
+        } catch (e) {
+          console.log(e);
+          this.loading = false;
+        }
       }
       const columns = this.data.columns;
-      const sheetHeaderName = []
+      const sheetHeaderName = [];
       for (let i = 0; i < columns.length; i++) {
         if (!columns[i].type) {
           this.sheetHeader.push(columns[i].name);
-          sheetHeaderName.push(columns[i].title)
+          sheetHeaderName.push(columns[i].title);
         }
       }
       this.sheetData = this.formatData(this.dataList);
@@ -138,13 +150,14 @@ export default {
           sheetHeader: sheetHeaderName
         }
       ];
+      this.loading = false;
       var toExcel = new ExportJsonExcel(this.option); //new
       toExcel.saveExcel(); //保
     },
     exportToExcel() {
-      this.dataList = []
-      this.sheetData = []
-      this.sheetHeader = []
+      this.dataList = [];
+      this.sheetData = [];
+      this.sheetHeader = [];
       this.getDataList();
     }
   }
