adaf912c7abe158e13d69c3d30404a6c4100993a
lixianliang <lixianliang@inframe.club>
2019-11-08 14:10:11 +0800

    优化组件search，获取url参数

--- src/components/app-table/search/Index.vue
+++ src/components/app-table/search/Index.vue
@@ -1,12 +1,12 @@
 <template>
   <el-form :model="query" :ref="formName" class="app-table-search">
     <app-form-item
-      v-for="(field, index) in fields"
-      :showLabel="false"
-      :key="`form-item${ index }`"
-      v-model.trim="query[field.name]"
-      :field="field"
-      class="app-form-item"
+        v-for="(field, index) in fields"
+        :showLabel="false"
+        :key="`form-item${ index }`"
+        v-model.trim="query[field.name]"
+        :field="field"
+        class="app-form-item"
     />
     <slot name="formItem" />
     <div class="app-form-button">
@@ -15,68 +15,88 @@
   </el-form>
 </template>
 <script>
-export default {
-  name: 'AppTableSearch',
-  props: {
-    fields: {
-      type: Array,
-      default() {
-        return []
-      }
-    },
-    value: {
-      type: Object,
-      default() {
-        return {}
-      }
-    },
-    showButton: {
-      type: Boolean,
-      default() {
-        return false
-      }
-    },
-    formName: {
-      type: String
-    },
-    type: {
-      type: String
-
-    }
-
-  },
-  data() {
-    return {
-      // rules: []
-    }
-  },
-  computed: {
-    query: {
-      set(params) {
-        this.$emit('input', { ...params })
-      },
-      get() {
-        return this.value
-      }
-    },
-    rules() {
-      const fields = this.fields
-      const rules = {}
-      fields.forEach((v, i) => {
-        if (v.required) {
-          const name = v.name
-          rules[name] = [
-            {
-              required: true,
-              message: '请输入' + v.label
-            }
-          ]
+  export default {
+    name: 'AppTableSearch',
+    props: {
+      fields: {
+        type: Array,
+        default() {
+          return []
         }
-      })
-      return rules
+      },
+      value: {
+        type: Object,
+        default() {
+          return {}
+        }
+      },
+      showButton: {
+        type: Boolean,
+        default() {
+          return false
+        }
+      },
+      formName: {
+        type: String
+      },
+      type: {
+        type: String
+
+      }
+
+    },
+    data() {
+      return {
+        // rules: []
+      }
+    },
+    computed: {
+      query: {
+        set(params) {
+          this.$emit('input', { ...params })
+        },
+        get() {
+          return this.value
+        }
+      },
+      rules() {
+        const fields = this.fields
+        const rules = {}
+        fields.forEach((v, i) => {
+          if (v.required) {
+            const name = v.name
+            rules[name] = [
+              {
+                required: true,
+                message: '请输入' + v.label
+              }
+            ]
+          }
+        })
+        return rules
+      }
+    },
+    mounted() {
+      this.initQuery();
+    },
+    methods: {
+      /**
+       * url参数，初始化到列表params
+       */
+      initQuery() {
+        const urlQuery = this.$route.query;
+        const keys = Object.keys(urlQuery);
+        const values = Object.values(urlQuery);
+        keys.forEach((k, i) => {
+          const val = values[i]
+          if (/^\d+$/ig.test(val)) {
+            urlQuery[k] = parseInt(val);
+          }
+        })
+        this.query = { ...this.query, ...urlQuery };
+      }
     }
   }
-}
 </script>
 <style lang="scss" scoped>
   .app-table-search {
@@ -88,4 +108,3 @@ export default {
     }
   }
 </style>
-
