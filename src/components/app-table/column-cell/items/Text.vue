599c242e11385a03e9d556a0deff9fdd09f0ab5b
lixianliang <lixianliang@inframe.club>
2020-11-26 00:24:11 +0800

    fix: 修改表格cell字长度问题

--- src/components/app-table/column-cell/items/Text.vue
+++ src/components/app-table/column-cell/items/Text.vue
@@ -1,35 +1,35 @@
 <template>
-<div>
-  <div v-if="textLength < 12" class="cell-text">
-    {{ text | filter(record, column) }}
+  <div>
+    <div v-if="textLength < 12" class="cell-text">
+      {{ text | filter(record, column) }}
+    </div>
+    <el-popover
+      v-else
+      placement="top-start"
+      width="200"
+      trigger="hover"
+      :content="text">
+      <span slot="reference">{{ text.substring(0,9) }}...</span>
+    </el-popover>
   </div>
-  <el-popover
-    v-else
-    placement="top-start"
-    width="200"
-    trigger="hover"
-    :content="text">
-    <span slot="reference">{{ text.substring(0,9) }}...</span>
-  </el-popover>
-</div>
 </template>
 
 <script>
-import Mixins from '../mixins';
-export default {
-  name: 'ColumnCellText',
-  mixins: [Mixins],
-  computed: {
-    textLength () {
-      const name = this.record[this.column.name]
-      return typeof name === 'string' ? name.length : 1
-    },
-    text () {
-      const val = this.record[this.column.name];
-      return val === undefined ?  '-' : val;
+  import Mixins from '../mixins';
+  export default {
+    name: 'ColumnCellText',
+    mixins: [Mixins],
+    computed: {
+      textLength () {
+        return this.text.length;
+      },
+      text () {
+        const val = this.record[this.column.name];
+        const filter = this.column.filter;
+        return val === undefined ?  '-' : (filter ? filter(val, this.record) : val);
+      }
     }
   }
-}
 </script>
 
 <style lang="scss" scoped>
